e9933591eaa58f2b7329bc64aa0cf9c78571e6f1
dvy19 <divyadixit.311204@gmail.com>
2026-06-14 21:06:14 +0530

    changes in urls.py

--- city/urls.py
+++ city/urls.py
@@ -1,12 +1,11 @@
 
 from django.urls import path
 
-from .views import CityView, LoginView, RegisterView
+from .views import CityView
 
 urlpatterns = [
 
     path("cities/", CityView.as_view()),
     path("cities/<int:pk>/", CityView.as_view()),
     
-
 ]
