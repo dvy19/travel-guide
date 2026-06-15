10a3eea9867855212e6fa8f29b048984de0139c7
dvy19 <divyadixit.311204@gmail.com>
2026-06-15 12:28:26 +0530

    change in places/views

--- places/views.py
+++ places/views.py
@@ -1,9 +1,9 @@
-from tkinter import Place
 
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from places.models import Place
 from places.serializers import PlaceCategorySerializer, PlaceSerializer, PlaceDetailSerializer
 
 
