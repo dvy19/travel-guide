7525f5fd8bb9969fe1df9f23b5dc7fcdb310329a
dvy19 <divyadixit.311204@gmail.com>
2026-06-16 21:16:15 +0530

    change in city/views

--- city/views.py
+++ city/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -10,63 +9,6 @@ from city.serializers import CitySerializer
 # Create your views here.
 
 class CityView(APIView):
-
-
-    def post(self,request):
-
-        serializer=CitySerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                {
-                    "message": "City created successfully",
-                    "data": serializer.data
-                },
-                status=status.HTTP_201_CREATED
-                )
-        
-        else:
-            return Response(
-                {
-                    "message": "City creation failed",
-                    "data": serializer.errors
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        
-    def put(self,request,pk):
-
-        try:
-            city=City.objects.get(pk=pk)
-        except City.DoesNotExist:
-            return Response(
-                {
-                    "message": "City not found"
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
-        
-        serializer=CitySerializer(city,data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                {
-                    "message": "City updated successfully",
-                    "data": serializer.data
-                },
-                status=status.HTTP_200_OK
-            )
-        
-        else:
-            return Response(
-                {
-                    "message": "City update failed",
-                    "data": serializer.errors
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
         
     def get(self, request, pk=None):
 
